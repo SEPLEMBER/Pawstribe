97f7e835b9916460d9aae6896322345a7a1eb02b
SEPLEMBER <216141135+SEPLEMBER@users.noreply.github.com>
2025-10-20 10:03:27 +0700

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "app.pawstribe.assistant"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "app.pawstribe.assistant"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 2
+        versionName = "0.4.0"
+        multiDexEnabled = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = null
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.bcprov.jdk15on)
+
+    implementation(libs.multidex)
+    implementation(libs.recyclerview)
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+
+    implementation("androidx.preference:preference-ktx:1.2.1")
+
+
+    implementation("androidx.documentfile:documentfile:1.0.1")
+
+    implementation("androidx.activity:activity-ktx:1.9.3")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
